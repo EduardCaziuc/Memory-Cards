a1746026f8e724fcc9dfba058ec5cedcdcf06498
Eduard Caziuc <caziuc.eduard@yahoo.com>
2018-04-25 18:51:02 +0300

    Initial Commit

---
+++ Memory Cards/MemoryCardsModel.swift
@@ -0,0 +1,110 @@
+//
+//  MemoryCardsModel.swift
+//  Memory Cards
+//
+//  Created by Eduard Caziuc on 18/03/2018.
+//  Copyright © 2018 Eduard Caziuc. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryCardsModel {
+   
+    private(set) var cards = [Card]() //or var cards = [Card]()
+     var matches = 0
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+       
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    }else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
+    
+    mutating func resetCards() {
+        cards.removeAll()
+    }
+    
+    mutating func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Memory Cards.chosenCard(at: \(index)): chosen index not in the cards")
+        
+        
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                if cards[matchIndex] == cards[index] {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                    matches += 1
+                }
+                
+                cards[index].isFaceUp = true
+                
+            }else {
+                //either no cards or 2 cards are face up
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+
+
+    
+    init(numberOfPairsOfCards: Int) {
+      assert(numberOfPairsOfCards > 0, "Memory Cards.init( \(numberOfPairsOfCards)): you must have at least one pair of cards")
+        
+        var ShuffledCards = [Card]()
+        
+        for _ in 1...numberOfPairsOfCards {
+            
+            let card = Card()
+            
+             ShuffledCards += [card, card]
+            
+            //or we can type: cards += [card, card] instead of ^
+        }
+        
+        // TODO: Shufle the cards (HomeWork)
+        for _ in ShuffledCards {
+            let randomIndex = ShuffledCards.count.arc4random
+            let randomCard = ShuffledCards.remove(at: randomIndex)
+            cards.append(randomCard)
+            //            print(randomCard)
+        }
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Memory Cards/ViewController.swift
+++ Memory Cards/ViewController.swift
@@ -9,17 +9,123 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        restartButton.isHidden = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var restartButton: UIButton!
+    
+    let card = Card()
+    
+    @IBAction func restartButtonPressed(_ sender: UIButton) {
+        restartButton.isHidden = true
+        
+        game = MemoryCardsModel(numberOfPairsOfCards: numberOfPairsOfCards)
+        game.matches = 0
+        updateViewFromModel()
+        flipCount = 0
+        updateFlipCountLabel()
+        
+        
     }
-
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    private lazy var game = MemoryCardsModel(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    var numberOfPairsOfCards: Int {
+       
+        return (cardButtons.count + 1) / 2
+        
+    }
+       
+    private(set) var flipCount: Int = 0 {
+        
+        didSet {
+            
+            updateFlipCountLabel()
+        }
+    }
+    
+    private func updateFlipCountLabel() {
+        let attributes: [NSAttributedStringKey: Any] = [
+            .strokeWidth: 5.0,
+            .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
+    
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
+    
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+    @IBAction private func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.index(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        }else {
+            print("The chosen card was not in the 'cardButtons' array")
+        }
+        if game.matches == numberOfPairsOfCards {
+            restartButton.isHidden = false
+        }
+        flipCount += 1
+    }
+    
+    
+    private func updateViewFromModel() {
+        for index in cardButtons.indices {
+           let button = cardButtons[index]
+           let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.isEnabled = false
+            }else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.isEnabled = true
+            }
+        }
+    }
+    
+    private var emojiChoices = ["👻","🎃", "👹", "🕷", "🕸","🦉","🦇", "🐈", "🧟‍♀️", "🐸"]
+    
+    private var emoji: [Card : String] = [ : ]
+    
+    private func emoji(for card: Card) -> String {
+        
+            if emoji[card] == nil, emojiChoices.count > 0 {
+             
+            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            }
+        return emoji[card] ?? "?"
+    }
+        
 }
+        
+    
+    
+    extension Int {
+        var arc4random: Int {
+            if self > 0 {
+                return Int(arc4random_uniform(UInt32(self)))
+            }else if self < 0 {
+                return -Int(arc4random_uniform(UInt32(abs(self))))
+            }else {
+                return 0
+            }
+        }
+    
+    }
 
